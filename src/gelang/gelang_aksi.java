1677cf4195ff4d2c6033ba9bc04f93585ab2acbd
kusairi <riskadelina96@gmail.com>
2018-03-27 17:20:41 +0800

    membuat class aksi dan method main

---
+++ src/gelang/gelang_aksi.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gelang;
+
+/**
+ *
+ * @author user
+ */
+public class gelang_aksi {
+    public static void main(String[] args) {
+        
+        
+        
+        
+    }
+    
+}
