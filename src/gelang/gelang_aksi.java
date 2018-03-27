c8acc35049b30a3c22f0b9b00ca484f0650826d1
kusairi <riskadelina96@gmail.com>
2018-03-27 17:36:07 +0800

    mengisi nilai atribut

--- src/gelang/gelang_aksi.java
+++ src/gelang/gelang_aksi.java
@@ -14,6 +14,34 @@ public class gelang_aksi {
         
         
         
+        gelang_class arab = new gelang_class();
+        System.out.println("gelang arab");
+        arab.nama="bapukah";
+        arab.jenis="kayu bapukah";
+        arab.harga=250000;
+        arab.ukuran="L";
+        arab.bahan="kayu kasturi";
+        arab.aksesoris();
+        
+        gelang_class rantau = new gelang_class();
+        System.out.println("gelang rantau");
+        rantau.nama="perak";
+        rantau.jenis="almunium";
+        rantau.harga=350000;
+        rantau.ukuran="S";
+        rantau.bahan="almunium Baja";
+        rantau.aksesoris();
+        
+        gelang_class indonesia = new gelang_class();
+        System.out.println("gelang indonesia");
+        indonesia.nama="batik";
+        indonesia.jenis="rotan";
+        indonesia.harga=300000;
+        indonesia.ukuran="L";
+        indonesia.bahan="kayu mahoni";
+        indonesia.aksesoris();
+        
+        
         
     }
     
